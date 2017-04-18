7fe74c025347e6e9a4a1af7a28e9d0a2febaafeb
Chris Jones <cmsj@tenshu.net>
2017-04-18 21:04:16 +0100

    Attempt to get docs to work with GitHub Pages easily

--- build_docs.sh
+++ build_docs.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-mkdir -p Docs
-../hammerspoon/scripts/docs/bin/build_docs.py -e ..//hammerspoon/scripts/docs/templates/ -o Docs -j -t -n Source
+mkdir -p .docs_tmp
+../hammerspoon/scripts/docs/bin/build_docs.py -e ..//hammerspoon/scripts/docs/templates/ -o .docs_tmp -j -t -n Source
+cp ..//hammerspoon/scripts/docs/templates/{docs.css,jquery.js} .docs_tmp/html/
+mv .docs_tmp/html/* docs/
