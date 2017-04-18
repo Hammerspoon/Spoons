38a107b04a620b7a47023b5ea6bea49d15c2fc93
Chris Jones <cmsj@tenshu.net>
2017-04-18 21:22:02 +0100

    Use the new index page title feature of build_docs.py

--- build_docs.sh
+++ build_docs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir -p .docs_tmp
-../hammerspoon/scripts/docs/bin/build_docs.py -e ..//hammerspoon/scripts/docs/templates/ -o .docs_tmp -j -t -n Source
+../hammerspoon/scripts/docs/bin/build_docs.py -e ..//hammerspoon/scripts/docs/templates/ -o .docs_tmp -i "Hammerspoon Spoons" -j -t -n Source
 cp ..//hammerspoon/scripts/docs/templates/{docs.css,jquery.js} .docs_tmp/html/
 mv .docs_tmp/html/* docs/
 mv .docs_tmp/docs{,_index}.json docs/
