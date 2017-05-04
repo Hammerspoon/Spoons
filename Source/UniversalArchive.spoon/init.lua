04ae80e85c2d386710b5e9eff1785598f152d3c9
Diego Zamboni <diego@zzamboni.org>
2017-05-04 12:51:16 +0200

    First working version of UniversalArchive spoon
    - Added generic application handler capability, new applications
      can be registered using the `registerApplication()` method.
    - Generic-destination hotkeys available for all handlers (not
      all handlers implement it).
    - Updated documentation.

--- Source/UniversalArchive.spoon/init.lua
+++ Source/UniversalArchive.spoon/init.lua
@@ -4,9 +4,19 @@
 ---
 --- Download: [https://github.com/Hammerspoon/Spoons/raw/master/Spoons/UniversalArchive.spoon.zip](https://github.com/Hammerspoon/Spoons/raw/master/Spoons/UniversalArchive.spoon.zip)
 ---
---- Currently Evernote, Mail.app and Outlook are supported.
---- For Evernote, also allows specifying other keybindings for
---- archiving directly to different notebooks.
+--- Using this Spoon enables you to use a single hotkey to archive an
+--- item in multiple applications.  Out of the box the following
+--- applications are supported: Evernote, Mail, Spark and Outlook. You
+--- can easily register handlers for new applications (or override the
+--- built-in ones) using the `registerApplication()` method. If you
+--- write a new handler and feel others could benefit from it, please
+--- submit a pull request!
+---
+--- Handlers can also provide support for archiving to multiple
+--- locations, and you can bind different hotkeys for each
+--- destination. At the moment only Evernote supports this. See the
+--- documentation for `bindHotkeys()` for the details on how to
+--- specify multiple-destination filing hotkeys.
 
 local obj={}
 obj.__index = obj
@@ -38,9 +48,21 @@ obj.outlook_archive_folder = "Archive"
 --- Boolean indicating whether a notification should be produced when an item is archived. Defaults to "true".
 obj.archive_notifications = true
 
--- Do not change this unless you know what you are doing
+--- UniversalArchive.evernote_delay_before_typing
+--- Variable
+--- In Evernote, archive is done by selectin "Move note" and then
+--- simulating typing of the notebook name. A short pause in between
+--- makes it more reliable for Evernote to recognize the notebook name
+--- correctly. This variable controls how much to wait, in seconds.
+--- Do not change this unless you know what you are doing
 obj.evernote_delay_before_typing = 0.2
 
+--- UniversalArchive:evernoteArchive(where)
+--- Method
+--- Archive current note in Evernote.
+---
+--- Parameters:
+---  * where - destination notebook. Defaults to the value of `UniversalArchive.evernote_archive_notebook`.
 function obj:evernoteArchive(where)
    local ev = hs.appfinder.appFromName("Evernote")
    -- Archiving Evernote notes
@@ -56,9 +78,15 @@ function obj:evernoteArchive(where)
    else
       hs.notify.show("Hammerspoon", "", "Something went wrong, couldn't find Evernote's menu item for archiving")
    end
+   return self
 end
 
---- Archive current message in Mail.app
+--- UniversalArchive:mailArchive()
+--- Method
+--- Archive current message in Mail using the built-in Archive functionality
+---
+--- Parameters:
+---  * none
 function obj:mailArchive()
    local mail = hs.appfinder.appFromName("Mail")
    if mail:selectMenuItem({"Message", "Archive"}) then
@@ -68,9 +96,15 @@ function obj:mailArchive()
    else
       hs.notify.show("Hammerspoon", "", "Something went wrong, couldn't find Mail's menu item for archiving")
    end
+   return self
 end
 
---- Archive current message in Spark
+--- UniversalArchive:sparkArchive()
+--- Method
+--- Archive current message in Spark using the built-in Archive functionality
+---
+--- Parameters:
+---  * none
 function obj:sparkArchive()
    local spark = hs.appfinder.appFromName("Spark")
    if spark:selectMenuItem({"Message", "Archive"}) then
@@ -80,10 +114,21 @@ function obj:sparkArchive()
    else
       hs.notify.show("Hammerspoon", "", "Something went wrong, couldn't find Spark's menu item for archiving")
    end
+   return self
 end
 
---- Archive current message in Outlook
-function obj:outlookArchive(where)
+--- UniversalArchive:outlookArchive()
+--- Method
+--- Archive current message in Outlook to the folder specified in
+--- `UniversalArchive.outlook_archive_folder`. The folder has to
+--- appear in the Message -> Move submenu for this to work. Since this
+--- submenu only lists the last few destination folders, you have to
+--- move a message by hand the first time (or periodically if you
+--- don't archive very often).
+---
+--- Parameters:
+---  * none
+function obj:outlookArchive()
    local outlook = hs.appfinder.appFromName("Microsoft Outlook")
    if outlook:selectMenuItem({"Message", "Move", self.outlook_archive_folder}) then
       if self.archive_notifications then
@@ -92,49 +137,45 @@ function obj:outlookArchive(where)
    else
       hs.notify.show("Hammerspoon", "", "Something went wrong, couldn't find Outlook's menu item for archiving")
    end
+   return self
 end
 
-function obj:universalArchive(where)
-   local ev = hs.appfinder.appFromName("Evernote")
-   local mail = hs.appfinder.appFromName("Mail")
-   local spark = hs.appfinder.appFromName("Spark")
-   local outlook = hs.appfinder.appFromName("Microsoft Outlook")
-
-   if ev ~= nil and ev:isFrontmost() then
-      -- Archiving Evernote notes
-      self:evernoteArchive(where)
-   elseif mail ~= nil and mail:isFrontmost() then
-      -- Archiving Mail messages
-      self:mailArchive()
-   elseif spark ~= nil and spark:isFrontmost() then
-      -- Archiving Mail messages in Spark
-      self:sparkArchive()
-   elseif outlook ~= nil and outlook:isFrontmost() then
-      -- Archiving Outlook messages
-      self:outlookArchive()
-   else
-      hs.notify.show("Hammerspoon", "", "I don't know how to archive in " .. hs.application.frontmostApplication():name())
-   end
-end
-
---- WindowManipulation:bindHotkeysToSpec(def, map)
+--- UniversalArchive:universalArchive(where)
 --- Method
---- Map a number of hotkeys according to a definition table
---- *** This function should be in a separate spoon or (preferably) in an hs.spoon module. I'm including it here for now to make the Spoon self-sufficient.
+--- Main entry point for archiving an item. If a handler function is
+--- defined for the current application, it is called with the
+--- `UniversalArchive` object as its first argument, and the archive
+--- destination (if provided) as the second. Handlers must have a
+--- "default destination" that gets used when no destination is
+--- provided. Not all handlers support specifying a destination. New
+--- handlers can be registered using the `registerApplication()`
+--- method.
+function obj:universalArchive(where)
+   local app = hs.application.frontmostApplication()
+   if app ~= nil then
+      local appname = app:name()
+      if self.archive_functions[appname] ~= nil then
+         self.archive_functions[appname](self, where)
+      else
+         hs.notify.show("Hammerspoon", "", "I don't know how to archive in " .. appname)
+      end
+   end
+   return self
+end
+
+--- UniversalArchive:registerApplication(appname, fn)
+--- Method
+--- Register a handler function for an application.
 ---
 --- Parameters:
----  * def - table containing name-to-function definitions for the hotkeys supported by the Spoon. Each key is a hotkey name, and its value must be a function that will be called when the hotkey is invoked.
----  * map - table containing name-to-hotkey definitions, as supported by [bindHotkeys in the Spoon API](https://github.com/Hammerspoon/hammerspoon/blob/master/SPOONS.md#hotkeys). Not all the entries in `def` must be bound, but if any keys in `map` don't have a definition, an error will be produced.
-function obj:bindHotkeysToSpec(def,map)
-   for name,key in pairs(map) do
-      if def[name] ~= nil then
-         if self._keys[name] then
-            self._keys[name]:delete()
-         end
-         self._keys[name]=hs.hotkey.bindSpec(key, def[name])
-      else
-         self.logger.ef("Error: Hotkey requested for undefined action '%s'", name)
-      end
+---  * appname - string containing the name of the application. If the application already has a handler, it will be replaced with the new one.
+---  * fn - handler function (to remove the handler for an application, use `nil`). The function receives the following arguments:
+---    * self - the UniversalArchive object, so the handler can make use of all the object methods and variables.
+---    * where - optional "destination" for the archive operation. Handlers must provide a default destination when `where == nil`. Destination doesn't make sense for all applications, so the implementation of this is optional and depending on the handler.
+function obj:registerApplication(appname, fn)
+   if appname then
+      self.archive_functions[appname] = fn
+      self.logger.f("Registered handler for application '%s'", appname)
    end
    return self
 end
@@ -144,23 +185,39 @@ end
 --- Binds hotkeys for UniversalArchive
 ---
 --- Parameters:
----  * mapping - A table containing hotkey objifier/key details for the following items:
----   * hello - Say Hello
+---  * mapping - A table containing hotkey modifier/key details for the following items:
+---   * `archive` - hotkey for trigger the `universalArchive()` method, to archive the current item on the current application.
+---   * `<app>_<dest>` - if an application handler supports multiple destinations, you can specify hotkeys for specific destinations using this format. For example, to assign a hotkey that files the current note in Evernote to the "MyProject" notebook using Ctrl-Alt-Cmd-M, you would specify `Evernote_MyProject = { { "ctrl", "alt", "cmd" }, "m" }` as one of the elements of `mapping`. Keep in mind that the application name must appear exactly as the system sees it (including upper/lowercase), and that if either the application or the destination name contain spaces or other non-alphanumeric characters, you need to use the Lua table notation. For example: `["Evernote_Some Long Notebook Name"] = { keybinding }`. At the moment only the Evernote handler supports multiple destinations.
 function obj:bindHotkeys(mapping)
-   self:bindHotkeysToSpec({archive = function() self:universalArchive() end}, mapping)
-   for a,k in pairs(mapping) do
-      local _,_,notebook = string.find(a, "^evernote_(.+)")
-      if notebook ~= nil then
-         if self._keys[a] then
-            self._keys[a]:delete()
+   -- Set up the universal hotkey
+   if mapping.archive then
+      if self._keys.archive then
+         self._keys.archive:delete()
+      end
+      self._keys.archive = hs.hotkey.bindSpec(mapping.archive, function() self:universalArchive() end)
+   end
+   -- Set up app-specific hotkeys for specific destinations
+   for app, fn in pairs(self.archive_functions) do
+      for a,k in pairs(mapping) do
+         local _,_,dest = string.find(a, "^" .. app .. "_(.+)")
+         if dest ~= nil then
+            if self._keys[a] then
+               self._keys[a]:delete()
+            end
+            self._keys[a] = hs.hotkey.bindSpec(k, function() fn(self,dest) end)
          end
-         self._keys[a] = hs.hotkey.bindSpec(k, function() self:evernoteArchive(notebook) end)
       end
    end
 end
 
 function obj:init()
    self._keys = {}
+   self.archive_functions = {
+      Evernote = obj.evernoteArchive,
+      Mail     = obj.mailArchive,
+      Spark    = obj.sparkArchive,
+      ["Microsoft Outlook"] = obj.outlookArchive,
+   }
 end
 
 return obj
