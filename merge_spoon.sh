8ca9b4c4cba0eb439dc371123dad698da45f476f
Chris Jones <cmsj@tenshu.net>
2017-06-06 21:53:57 +0100

    Add the script I'm using to merge spoons. This will be useless unless your computer is setup the same as mine!

---
+++ merge_spoon.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ "$1" == "" -o "$2" == "" ]; then
+    echo "Usage: $0 GITHUB_PR_ID SPOON_NAME"
+    exit 1
+fi
+
+set -eu
+
+PR="$1"
+SPOON="$2"
+
+hub am -3 https://github.com/Hammerspoon/Spoons/pull/${PR}
+../hammerspoon/scripts/docs/bin/build_docs.py -e ../hammerspoon/scripts/docs/templates/ -o Source/${SPOON}.spoon/ -j -n Source/${SPOON}.spoon/
+rm Source/${SPOON}.spoon/docs_index.json
+git commit -am "Generate docs for ${SPOON}"
+make
+git add Spoons/${SPOON}.spoon.zip
+git commit -am "Add binary package for ${SPOON}. Closes #${PR}"
+./build_docs.sh
+git add docs
+git commit -am "Update docs"
+git push
+
