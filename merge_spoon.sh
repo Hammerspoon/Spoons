37a2fe1ec9fb7c30861b9b901b7b115df3b34a53
Chris Jones <cmsj@tenshu.net>
2017-09-14 14:03:09 -0600

    Generate docs for SendToOmniFocus

--- merge_spoon.sh
+++ merge_spoon.sh
@@ -5,12 +5,12 @@ if [ "$1" == "" -o "$2" == "" ]; then
     exit 1
 fi
 
-set -eu
 
 PR="$1"
 SPOON="$2"
 
 hub am -3 https://github.com/Hammerspoon/Spoons/pull/${PR}
+set -eu
 ../hammerspoon/scripts/docs/bin/build_docs.py -e ../hammerspoon/scripts/docs/templates/ -o Source/${SPOON}.spoon/ -j -n Source/${SPOON}.spoon/
 rm Source/${SPOON}.spoon/docs_index.json
 git commit -am "Generate docs for ${SPOON}"
@@ -20,5 +20,5 @@ git commit -am "Add binary package for ${SPOON}. Closes #${PR}"
 ./build_docs.sh
 git add docs
 git commit -am "Update docs"
-git push
+#git push
 
