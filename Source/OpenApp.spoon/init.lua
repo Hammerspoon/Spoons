a1191ff3fa54bc21b407550c11e169ed8cc3abd1
Yuan Fu <casouri@gmail.com>
2018-04-10 23:54:28 -0400

    Add OpenApp

---
+++ Source/OpenApp.spoon/init.lua
@@ -0,0 +1,59 @@
+--- === OpenApp ===
+---
+--- open a chooser to open apps.
+
+-- Metadata
+obj.name = "OpenApp"
+obj.version = "0.7"
+obj.author = "Yuan Fu <casouri@gmail.com>"
+obj.homepage = "https://github.com/Hammerspoon/Spoons"
+obj.license = "MIT - https://opensource.org/licenses/MIT"
+
+obj = {}
+obj.__index = obj
+
+--- OpenApp.searchPath
+--- Variable
+--- OpenApp search paths in this list for applications.
+obj.searchPath = {'/Applications'}
+
+--- OpenApp.show()
+--- Function
+--- Call this function to pop chooser to choose applications.
+function obj.show()
+   obj.chooser:show()
+end
+
+local function getAppList()
+   local appList = {}
+   for index = 1, #obj.searchPath do
+      -- resolve relative path and symlinks
+      local path = hs.fs.pathToAbsolute(obj.searchPath[index])
+      local iterFunc, dicTable = hs.fs.dir(path)
+      while true do
+         local fileName = iterFunc(dicTable)
+         -- when there are no other files
+         if not fileName then break end
+         if fileName ~= '.' and fileName ~= '..' then
+            table.insert(appList, {text=fileName})
+         end
+      end
+   end
+   return appList
+end
+
+local function openApp(appTable)
+   obj.chooser:hide()
+   if appTable then
+      hs.application.launchOrFocus(appTable.text)
+   end
+end
+
+function obj:init()
+   obj.chooser = hs.chooser.new(openApp)
+   obj.chooser:choices(getAppList)
+   obj.chooser:searchSubText(true)
+   return obj.chooser
+end
+
+return obj
