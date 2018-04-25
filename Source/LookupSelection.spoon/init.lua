8522909323d136cae25a2d115747c6a1e35391c8
Alf Schilken <alfred@schilken.de>
2018-04-25 11:06:08 +0200

    update description

--- Source/LookupSelection.spoon/init.lua
+++ Source/LookupSelection.spoon/init.lua
@@ -17,26 +17,26 @@ obj.homepage = "https://github.com/Hammerspoon/Spoons"
 obj.license = "MIT - https://opensource.org/licenses/MIT"
 
 -- User-configurable variables
+-- may used later --
+-- LookupSelection.popup_size
+-- Variable
+-- `hs.geometry` object representing the size to use for the translation popup window. Defaults to `hs.geometry.size(770, 610)`.
+--obj.popup_size = hs.geometry.size(770, 610)
 
---- LookupSelection.popup_size
---- Variable
---- `hs.geometry` object representing the size to use for the translation popup window. Defaults to `hs.geometry.size(770, 610)`.
-obj.popup_size = hs.geometry.size(770, 610)
+-- LookupSelection.popup_style
+-- Variable
+-- Value representing the window style to be used for the translation popup window. This value needs to be a valid argument to [`hs.webview.setStyle()`](http://www.hammerspoon.org/docs/hs.webview.html#windowStyle) (i.e. a combination of values from [`hs.webview.windowMasks`](http://www.hammerspoon.org/docs/hs.webview.html#windowMasks[]). Default value: `hs.webview.windowMasks.utility|hs.webview.windowMasks.HUD|hs.webview.windowMasks.titled|hs.webview.windowMasks.closable`
+--obj.popup_style = hs.webview.windowMasks.utility|hs.webview.windowMasks.HUD|hs.webview.windowMasks.titled|hs.webview.windowMasks.closable
 
---- LookupSelection.popup_style
---- Variable
---- Value representing the window style to be used for the translation popup window. This value needs to be a valid argument to [`hs.webview.setStyle()`](http://www.hammerspoon.org/docs/hs.webview.html#windowStyle) (i.e. a combination of values from [`hs.webview.windowMasks`](http://www.hammerspoon.org/docs/hs.webview.html#windowMasks[]). Default value: `hs.webview.windowMasks.utility|hs.webview.windowMasks.HUD|hs.webview.windowMasks.titled|hs.webview.windowMasks.closable`
-obj.popup_style = hs.webview.windowMasks.utility|hs.webview.windowMasks.HUD|hs.webview.windowMasks.titled|hs.webview.windowMasks.closable
+-- LookupSelection.popup_close_on_escape
+-- Variable
+-- If true, pressing ESC on the popup window will close it. Defaults to `true`
+--obj.popup_close_on_escape = true
 
---- LookupSelection.popup_close_on_escape
---- Variable
---- If true, pressing ESC on the popup window will close it. Defaults to `true`
-obj.popup_close_on_escape = true
-
---- LookupSelection.popup_close_after_copy
---- Variable
---- If true, the popup window will close after translated text is copied to pasteboard. Defaults to `true`
-obj.popup_close_after_copy = false
+-- LookupSelection.popup_close_after_copy
+-- Variable
+-- If true, the popup window will close after translated text is copied to pasteboard. Defaults to `true`
+--obj.popup_close_after_copy = false
 
 
 --- LookupSelection.logger
