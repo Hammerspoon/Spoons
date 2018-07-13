95ef6571039df7de49eac1e12fb3d158f4b42873
Chris Jones <cmsj@tenshu.net>
2018-07-13 20:59:50 +0100

    Add travis config and a script to enforce various properties of Spoons

---
+++ travis-enforce.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -u
+
+# Enforce some basic things we want, in a Spoon
+FAILED=0
+
+pushd Source || exit
+
+# Check that there's a line containing: obj.version = ['"][0-9.]+['"]
+echo "Checking for obj.version conformance..."
+for spoon in * ; do
+    echo -n "  ${spoon}: "
+    VERSIONSTR=$(grep -E "obj.version = ['\"][0-9.]+['\"]" "${spoon}/init.lua" | awk '{ print $NF}')
+    if [ "${VERSIONSTR}" == "" ]; then
+        echo "UNKNOWN"
+        FAILED=1
+    else
+        echo "${VERSIONSTR}"
+    fi
+done
+echo ""
+
+if [ "${FAILED}" != "0" ]; then
+    echo "Some preceeding check failed."
+    exit 1
+fi
+
