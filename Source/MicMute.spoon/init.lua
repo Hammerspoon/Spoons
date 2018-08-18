38644e196f085201ad28eed20a61adbaf520b587
Casey Tucker <dctucker@github.com>
2018-08-17 23:01:54 -0700

    adding bindHotkeys method and more doc strings

--- Source/MicMute.spoon/init.lua
+++ Source/MicMute.spoon/init.lua
@@ -2,7 +2,7 @@
 ---
 --- Microphone Mute Toggle and status indicator
 ---
---- Download: []()
+--- Download: [https://github.com/dctucker/Spoons/raw/master/Spoons/MicMute.spoon.zip](https://github.com/dctucker/Spoons/raw/master/Spoons/MicMute.spoon.zip)
 
 local obj={}
 obj.__index = obj
@@ -25,6 +25,10 @@ function obj:updateMicMute(muted)
 	end
 end
 
+--- MicMute:toggleMicMute()
+--- Method
+--- Toggle mic mute on/off
+---
 function obj:toggleMicMute()
 	local mic = hs.audiodevice.defaultInputDevice()
 	local zoom = hs.application'Zoom'
@@ -52,7 +56,42 @@ function obj:toggleMicMute()
 	obj:updateMicMute(-1)
 end
 
+--- MicMute:bindHotkeys(mapping, latch_timeout)
+--- Method
+--- Binds hotkeys for MicMute
+---
+--- Parameters:
+---  * mapping - A table containing hotkey modifier/key details for the following items:
+---   * toggle - This will cause the microphone mute status to be toggled. Hold for momentary, press quickly for toggle.
+---  * latch_timeout - Time in seconds to hold the hotkey before momentary mode takes over, in which the mute will be toggled again when hotkey is released. Latch if released before this time. 0.75 for 750 milliseconds is a good value.
+function obj:bindHotkeys(mapping, latch_timeout)
+	if (self.hotkey) then
+		self.hotkey:delete()
+	end
+	local mods = mapping["toggle"][1]
+	local key = mapping["toggle"][2]
+
+	if latch_timeout then
+		self.hotkey = hs.hotkey.bind(mods, key, function()
+			self:toggleMicMute()
+			self.time_since_mute = hs.timer.secondsSinceEpoch()
+		end, function()
+			if hs.timer.secondsSinceEpoch() > self.time_since_mute + latch_timeout then
+				self:toggleMicMute()
+			end
+		end)
+	else
+		self.hotkey = hs.hotkey.bind(mods, key, function()
+			self:toggleMicMute()
+		end)
+	end
+
+	return self
+end
+
+
 function obj:init()
+	obj.time_since_mute = 0
 	obj.mute_menu = hs.menubar.new()
 	obj.mute_menu:setClickCallback(function()
 		obj:toggleMicMute()
