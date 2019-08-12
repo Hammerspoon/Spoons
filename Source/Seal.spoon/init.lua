Hammerspoon/Spoons

commit 276a0ac891f5b177c2be2c654e6ba83a40f7c56b

Add the ability for Seal:show()/:toggle() to pre-populate the query box

Source/Seal.spoon/init.lua
@@ -222,37 +222,39 @@ function obj:stop()
     return self
 end
 
---- Seal:show()
+--- Seal:show(query)
 --- Method
 --- Shows the Seal UI
 ---
 --- Parameters:
----  * None
+---  * query - An optional string to pre-populate the query box with
 ---
 --- Returns:
 ---  * None
 ---
 --- Notes:
 ---  * This may be useful if you wish to show Seal in response to something other than its hotkey
-function obj:show()
+function obj:show(query)
     self.chooser:show()
+    self.chooser:query(query)
     return self
 end
 
---- Seal:toggle()
+--- Seal:toggle(query)
 --- Method
 --- Shows or hides the Seal UI
 ---
 --- Parameters:
----  * None
+---  * query - An optional string to pre-populate the query box with
 ---
 --- Returns:
 ---  * None
-function obj:toggle()
+function obj:toggle(query)
     if self.chooser:isVisible() then
         self.chooser:hide()
     else
-        self.chooser:show()
+        self:show(query)
+
     end
     return self
 end

commit 14421441131bb01147a8871738eff65c783975e8

Fix regression to Seal introduced in 276a0ac
Restore previous search being present in Seal (when `query` isn't given).

Source/Seal.spoon/init.lua
@@ -236,7 +236,7 @@ end
 ---  * This may be useful if you wish to show Seal in response to something other than its hotkey
 function obj:show(query)
     self.chooser:show()
-    self.chooser:query(query)
+    if query then self.chooser:query(query) end
     return self
 end
 
@@ -254,7 +254,6 @@ function obj:toggle(query)
         self.chooser:hide()
     else
         self:show(query)
-
     end
     return self
 end