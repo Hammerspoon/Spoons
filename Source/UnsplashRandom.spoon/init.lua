dfb0357f9899bbf89d3c21f3e6eef030d771e6bc
gautamkrishnar <r.gautamkrishna@gmail.com>
2019-12-13 12:27:43 +0530

    Added UnsplashRandom spoon

---
+++ Source/UnsplashRandom.spoon/init.lua
@@ -0,0 +1,47 @@
+--- === UnsplashRandom ===
+---
+--- Automatically sets a random Unsplash image as your wallpaper daily.
+---
+--- Download: [https://github.com/Hammerspoon/Spoons/raw/master/Spoons/UnsplashRandom.spoon.zip](https://github.com/Hammerspoon/Spoons/raw/master/Spoons/UnsplashRandom.spoon.zip)
+
+local obj={}
+obj.__index = obj
+
+-- Metadata
+obj.name = "UnsplashRandom"
+obj.version = "1.0"
+obj.author = "Gautam Krishna R <r.gautamkrishna@gmail.com>"
+obj.homepage = "https://github.com/Hammerspoon/Spoons"
+obj.license = "MIT - https://opensource.org/licenses/MIT"
+
+local function curl_callback(exitCode, stdOut, stdErr)
+    if exitCode == 0 then
+        obj.task = nil
+        hs.screen.mainScreen():desktopImageURL("file://" .. obj.localpath)
+    else
+        print(stdOut, stdErr)
+    end
+end
+
+local function unsplashRequest()
+    if obj.task then
+        obj.task:terminate()
+        obj.task = nil
+    end
+    obj.localpath = os.getenv("HOME") .. "/.Trash/".. hs.hash.SHA1(hs.timer.absoluteTime()) .. ".jpg"
+    local screen_data = hs.screen.mainScreen():currentMode()
+    local image_url = "https://source.unsplash.com/random/" .. screen_data["w"]  .. "x" .. screen_data["h"]
+    obj.task = hs.task.new("/usr/bin/curl", curl_callback, {"-L", image_url, "-o", obj.localpath})
+    obj.task:start()
+end
+
+function obj:init()
+    if obj.timer == nil then
+        obj.timer = hs.timer.doEvery(3*60*60, function() unsplashRequest() end)
+        obj.timer:setNextTrigger(5)
+    else
+        obj.timer:start()
+    end
+end
+
+return obj
