7d210efa2de02b5d6be9ccdca476ca265e3b374c
Diego Zamboni <diego@zzamboni.org>
2020-06-06 21:00:16 +0200

    New spoon EjectVolumes
    Eject all non-internal disks. Can be triggered on sleep, using a menubar
    icon or a hotkey.

---
+++ Source/EjectVolumes.spoon/init.lua
@@ -0,0 +1,145 @@
+--- === EjectVolumes ===
+---
+--- Eject all non-internal disks. Can be triggered on sleep, using a menubar icon or a hotkey.
+---
+--- Download: [https://github.com/Hammerspoon/Spoons/raw/master/Spoons/EjectVolumes.spoon.zip](https://github.com/Hammerspoon/Spoons/raw/master/Spoons/EjectVolumes.spoon.zip)
+
+local obj={}
+obj.__index = obj
+
+-- Metadata
+obj.name = "EjectVolumes"
+obj.version = "0.1"
+obj.author = "Diego Zamboni <diego@zzamboni.org>"
+obj.homepage = "https://github.com/Hammerspoon/Spoons"
+obj.license = "MIT - https://opensource.org/licenses/MIT"
+
+-- Watcher for system sleep events
+obj.watcher = nil
+-- Menubar icon
+obj.menubar = nil
+
+--- EjectVolumes.logger
+--- Variable
+--- Logger object used within the Spoon. Can be accessed to set the default log level for the messages coming from the Spoon.
+obj.logger = hs.logger.new('EjectVolumes')
+
+--- EjectVolumes.never_eject
+--- Variable
+--- List containing volume paths that should never be ejected. Default value: empty list
+obj.never_eject = { }
+
+--- EjectVolumes.notify
+--- Variable
+--- Boolean, whether to produce a notification about the volumes that get ejected. Default value: `false`
+obj.notify = false
+
+--- EjectVolumes.eject_on_sleep
+--- Variable
+--- Boolean, whether to eject volumes before the system goes to sleep. Default value: true
+obj.eject_on_sleep = true
+
+--- EjectVolumes.show_in_menubar
+--- Variable
+--- Boolen, whether to show a menubar button to eject all drives. Default value: true
+obj.show_in_menubar = true
+
+--- EjectVolumes.other_eject_events
+--- Variable
+--- List of additional system events on which the volumes should be ejected. The
+--- values must be
+--- [http://www.hammerspoon.org/docs/hs.caffeinate.watcher.html](`hs.caffeinate.watcher`)
+--- constant values. Default value: empty list
+obj.other_eject_events = { }
+
+--- EjectVolumes:shouldEject(path, info)
+--- Method
+--- Determine if a volume should be ejected.
+---
+--- Parameters:
+---  * path - the mount path of the volume.
+---  * info - a table containing a data structure as returned by `hs.fs.volume.allVolumes()`.
+--- Returns:
+---  * A boolean indicating whether the volume should be ejected.
+function obj:shouldEject(path, info)
+  return not (hs.fnutils.contains(self.never_eject, path) or info["NSURLVolumeIsInternalKey"])
+end
+
+function obj.showNotification(title, subtitle, msg)
+  hs.notify.new(
+    {
+      title = title,
+      subTitle = subtitle,
+      informativeText = msg,
+      withdrawAfter = 0
+  }):send()
+end
+
+--- EjectVolumes:ejectVolumes()
+--- Method
+--- Eject all volume
+function obj:ejectVolumes()
+  local v = hs.fs.volume.allVolumes()
+  for path,info in pairs(v) do
+    if self:shouldEject(path,info) then
+      local result,msg = hs.fs.volume.eject(path)
+      if result then
+        if self.notify then
+          self.showNotification("EjectVolumes", "Volume " .. path .. " ejected.", "")
+        end
+        self.logger.f("Volume %s was ejected.", path)
+      else
+        self.showNotification("EjectVolumes", "Error ejecting " .. path, msg)
+        self.logger.ef("Error ejecting volume %s: %s", path, msg)
+      end
+    end
+  end
+  return self
+end
+
+--- EjectVolumes:bindHotkeys(mapping)
+--- Method
+--- Binds hotkeys for EjectVolumes
+---
+--- Parameters:
+---  * mapping - A table containing hotkey objifier/key details for the following items:
+---   * eject_volumes - eject all volumes.
+function obj:bindHotkeys(mapping)
+  local spec = { eject_volumes = hs.fnutils.partial(self.ejectVolumes, self) }
+  hs.spoons.bindHotkeysToSpec(spec, mapping)
+end
+
+--- EjectVolumes:start()
+--- Method
+--- Start the watcher for system sleep, to trigger volume ejection.
+function obj:start()
+  if self.eject_on_sleep then
+    self.watcher = hs.caffeinate.watcher.new(
+      function (e)
+        if self.eject_on_sleep or hs.fnutils.contains(self.other_eject_events, e) then
+          self:ejectVolumes()
+        end
+      end):start()
+  end
+  if self.show_in_menubar then
+    self.menubar = hs.menubar.new():
+      setIcon(hs.image.imageFromName("NSNavEjectButton.normal")):
+      setClickCallback(hs.fnutils.partial(self.ejectVolumes, self))
+  end
+end
+
+--- EjectVolumes:stop()
+--- Method
+--- Stop the watcher
+function obj:stop()
+  if obj.watcher then
+    obj.watcher:stop()
+    obj.watcher = nil
+  end
+  if obj.show_in_menubar then
+    self.menubar:delete()
+    self.menubar = nil
+  end
+end
+
+return obj
