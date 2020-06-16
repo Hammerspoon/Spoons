6c93bb7aa43fed3a2016f9964ce2cc0372f47ce7
Diego Zamboni <diego@zzamboni.org>
2020-06-16 22:01:11 +0200

    Integrated EjectMenu functionality
    Integrated the menubar-handling code from
    https://github.com/Hammerspoon/Spoons/pull/179, bringing the
    functionality of both new spoons into one:
    
    - Eject/eject all menu
    - Eject on hotkey
    - Eject on sleep/lid close

--- Source/EjectVolumes.spoon/init.lua
+++ Source/EjectVolumes.spoon/init.lua
@@ -1,6 +1,8 @@
 --- === EjectVolumes ===
 ---
---- Eject all non-internal disks. Can be triggered on sleep, on lid close, using a menubar icon or a hotkey.
+--- A much-needed eject menu for your Mac menu bar. Allows ejecting
+--- individual or all non-internal disks. Ejection can also be
+--- triggered on sleep, on lid close, or using a hotkey.
 ---
 --- Download: [https://github.com/Hammerspoon/Spoons/raw/master/Spoons/EjectVolumes.spoon.zip](https://github.com/Hammerspoon/Spoons/raw/master/Spoons/EjectVolumes.spoon.zip)
 
@@ -10,7 +12,7 @@ obj.__index = obj
 -- Metadata
 obj.name = "EjectVolumes"
 obj.version = "0.1"
-obj.author = "Diego Zamboni <diego@zzamboni.org>"
+obj.author = "Diego Zamboni <diego@zzamboni.org>, Mark Juers <mpjuers@gmail.com>"
 obj.homepage = "https://github.com/Hammerspoon/Spoons"
 obj.license = "MIT - https://opensource.org/licenses/MIT"
 
@@ -18,6 +20,8 @@ obj.license = "MIT - https://opensource.org/licenses/MIT"
 obj.caff_watcher = nil
 -- Watcher for screen change events
 obj.screen_watcher = nil
+-- Watcher for keyboard events (to change the menubar icon)
+obj.flags_watcher = nil
 -- Menubar icon
 obj.menubar = nil
 
@@ -73,20 +77,27 @@ function obj:shouldEject(path, info)
   return not (hs.fnutils.contains(self.never_eject, path) or info["NSURLVolumeIsInternalKey"])
 end
 
-function obj.showNotification(title, subtitle, msg)
+function obj.showNotification(title, subtitle, msg, persistent)
+  local withdraw_time = 5
+  if persistent then
+    withdraw_time = 0
+  end
   hs.notify.new(
     {
       title = title,
       subTitle = subtitle,
       informativeText = msg,
-      withdrawAfter = 0
+      withdrawAfter = withdraw_time
   }):send()
 end
 
 --- EjectVolumes:ejectVolumes()
 --- Method
---- Eject all volume
-function obj:ejectVolumes()
+--- Eject all volumes
+---
+--- Parameters
+---  * persistent_notifs: a boolean indicating whether notifications (if shown) should be persistent.
+function obj:ejectVolumes(persistent_notifs)
   local v = hs.fs.volume.allVolumes()
   self.logger.df("Ejecting volumes")
   for path,info in pairs(v) do
@@ -94,7 +105,7 @@ function obj:ejectVolumes()
       local result,msg = hs.fs.volume.eject(path)
       if result then
         if self.notify then
-          self.showNotification("EjectVolumes", "Volume " .. path .. " ejected.", "")
+          self.showNotification("EjectVolumes", "Volume " .. path .. " ejected.", "", persistent_notifs)
         end
         self.logger.f("Volume %s was ejected.", path)
       else
@@ -106,16 +117,113 @@ function obj:ejectVolumes()
   return self
 end
 
+-- EjectVolumes:execMenuItem(mods, table)
+-- Method
+-- Defines and executes menu item based on which modifiers are held.
+--
+-- Parameters
+-- * mods: A table containing which modifiers are held in {key = bool} format
+--   only if 'bool' is true. Other modifiers are omitted.
+-- * table: The menu item being activated.
+function obj:execMenuItem (mods, table)
+  if (
+    mods['cmd'] == true and
+      mods['ctrl'] == false and
+      mods['alt'] == false and
+      mods['shift'] == false and
+      mods['fn'] == false
+  ) then
+    hs.osascript.applescript(
+      'tell application "Finder"'
+        .. ' to open ("' .. table['path'] .. '" as POSIX file)'
+    )
+    hs.appfinder.appFromName("Finder"):activate()
+  else
+    hs.fs.volume.eject(table['path'])
+    if self.notify then
+      self.showNotification("EjectVolumes", "Volume " .. path .. " ejected.", "", false)
+    end
+  end
+end
+
+-- EjectVolumes:initEjectMenu(mods)
+-- Method
+-- Initializes eject menu when clicked.
+--
+-- Parameters
+-- * mods: a table containing {mod = bool} for all modifiers, where bool can be
+--   be either 'true' or 'false' (unlike execMenuItem)
+--
+-- Returns
+-- ejectMenuTable: a table containing entries and functions for ejectable drives.
+function obj:initEjectMenu (mods)
+  local volumes = hs.fs.volume.allVolumes()
+  local ejectMenuDrives = {}
+  for path, v in pairs(volumes) do
+    if self:shouldEject(path, v) then
+      ejectMenuDrives[path] = v
+    end
+  end
+  local ejectMenuTable = {
+    {title = "Eject All",
+     fn = function () self:ejectVolumes(false) end,
+     disabled = (next(ejectMenuDrives) == nil)
+    },
+    {title = '-'}
+  }
+  verb = "Eject "
+  if (
+    mods['cmd'] == true and
+      mods['ctrl'] == false and
+      mods['alt'] == false and
+      mods['shift'] == false and
+      mods['fn'] == false
+  ) then
+    verb = "Open "
+  end
+  if pcall(function () next(ejectMenuDrives) end) then
+    for drive, v in pairs(ejectMenuDrives) do
+      self.logger.i(drive .. " is ejectable.")
+      table.insert(
+        ejectMenuTable,
+        {
+          title = drive,
+          path = drive,
+          fn = function (mods, table) self:execMenuItem(mods, table) end
+        }
+      )
+    end
+  else
+    self.logger.i("No external drives.")
+  end
+  return ejectMenuTable
+end
+
 --- EjectVolumes:bindHotkeys(mapping)
 --- Method
 --- Binds hotkeys for EjectVolumes
 ---
 --- Parameters:
 ---  * mapping - A table containing hotkey objifier/key details for the following items:
----   * eject_volumes - eject all volumes.
+---   * ejectAll - eject all volumes.
 function obj:bindHotkeys(mapping)
-  local spec = { eject_volumes = hs.fnutils.partial(self.ejectVolumes, self) }
+  local spec = { ejectAll = hs.fnutils.partial(self.ejectVolumes, self) }
   hs.spoons.bindHotkeysToSpec(spec, mapping)
+end
+
+-- EjectVolumes:changeEjectMenuIcon(mods)
+-- Method
+-- Changes eject menu icon depending on which modifiers are held.
+--
+-- Parameters
+-- * mods: A table containing for which the keys are the modifiers being held
+--   and the values are 'true'.
+function obj:changeEjectMenuIcon (mods)
+  if mods:containExactly({'cmd'}) then
+    self.menubar:setTitle('⮑')
+  elseif mods:containExactly({}) then
+    self.menubar:setTitle('⏏')
+  end
 end
 
 --- EjectVolumes:start()
@@ -128,9 +236,9 @@ function obj:start()
         self.logger.df("Received hs.caffeinate.watcher event %d", e)
         if self.eject_on_sleep or hs.fnutils.contains(self.other_eject_events, e) then
           self.logger.df("  About to go to sleep")
-          self:ejectVolumes()
+          self:ejectVolumes(true)
         end
-      end):start()
+    end):start()
   end
   if self.eject_on_lid_close then
     self.screen_watcher = hs.screen.watcher.new(
@@ -139,16 +247,20 @@ function obj:start()
         if (hs.fnutils.every(hs.screen.allScreens(),
                              function (s) return s:name() ~= "Color LCD" end)) then
           self.logger.df("  'Color LCD' display is gone")
-          self:ejectVolumes()
+          self:ejectVolumes(true)
         end
       end
     ):start()
   end
   if self.show_in_menubar then
-    self.menubar = hs.menubar.new():
-      setIcon(hs.image.imageFromName("NSNavEjectButton.normal")):
-      setClickCallback(hs.fnutils.partial(self.ejectVolumes, self))
+    self.menubar = hs.menubar.new():setTitle("⏏"):
+    setMenu(function (mods) return self:initEjectMenu(mods) end)
+    self.flags_watcher = hs.eventtap.new(
+      {hs.eventtap.event.types.flagsChanged},
+      function (event) self:changeEjectMenuIcon(event:getFlags()) end
+    ):start()
   end
+  return self
 end
 
 --- EjectVolumes:stop()
@@ -163,10 +275,15 @@ function obj:stop()
     self.screen_watcher:stop()
     self.screen_watcher = nil
   end
-  if obj.show_in_menubar then
+  if self.menubar then
     self.menubar:delete()
     self.menubar = nil
   end
+  if self.flags_watcher then
+    self.flags_watcher:stop()
+    self.flags_watcher = nil
+  end
+  return self
 end
 
 return obj
