53d4e29a2cdb088b9160f10411105956eba855dc
Jason Felice <jason.m.felice@gmail.com>
2021-07-22 16:53:42 -0400

    Rename some things

--- Source/WindowSigils.spoon/space_map.lua
+++ Source/WindowSigils.spoon/space_map.lua
@@ -64,22 +64,22 @@ function SpaceMap:_initialize(screens, windows)
   self:_build_occupied_map(windows)
 end
 
-function SpaceMap:_find_row_right_extent(row, left, j_end)
+function SpaceMap:_find_row_right_extent(row, left, screen_right)
   local row_right = left
-  while row_right + 1 <= j_end and not self.occupied[row][row_right + 1] do
+  while row_right + 1 <= screen_right and not self.occupied[row][row_right + 1] do
     row_right = row_right + 1
   end
   return row_right
 end
 
-function SpaceMap:_find_empty_extent(top, left, i_end, j_end)
+function SpaceMap:_find_empty_extent(top, left, screen_bottom, screen_right)
   local bottom = top
-  while bottom + 1 <= i_end and not self.occupied[bottom + 1][left] do
+  while bottom + 1 <= screen_bottom and not self.occupied[bottom + 1][left] do
     bottom = bottom + 1
   end
   local right = nil
-  for i = top, bottom do
-    local row_right = self:_find_row_right_extent(i, left, j_end)
+  for row = top, bottom do
+    local row_right = self:_find_row_right_extent(row, left, screen_right)
     if right == nil or row_right < right then
       right = row_right
     end
