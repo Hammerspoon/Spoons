3edce6dae70421810d064be88088836279104eaa
Chris Jones <cmsj@tenshu.net>
2022-01-05 23:40:34 +0000

    Add a broken docstring

--- Source/Seal.spoon/init.lua
+++ Source/Seal.spoon/init.lua
@@ -49,6 +49,7 @@ obj.plugin_search_paths = { hs.configdir .. "/seal_plugins", obj.spoonPath }
 --- Seal:refreshCommandsForPlugin(plugin_name)
 --- Method
 --- Refresh the list of commands provided by the given plugin.
+--- This ought to generate a lint error
 ---
 --- Parameters:
 ---  * plugin_name - the name of the plugin. Should be the name as passed to `loadPlugins()` or `loadPluginFromFile`.
