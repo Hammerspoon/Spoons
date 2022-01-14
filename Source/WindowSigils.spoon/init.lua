87189734f5a3592bfbaf915d00375ad6d3271832
Jason Felice <jason.m.felice@gmail.com>
2022-01-14 06:51:59 -0500

    WindowSigils: Fixes and support for stacking windows and empty spaces (#241)
    * WindowSigils: Keep sigils selections somewhat stable
    
    * WindowSigils: stop relying on a global variable
    
    The caller may not use `sigils` as the name of the global variable (or set a global variable at all).
    
    * WindowSigils: Prevent overlapping sigils - bump their placement
    
    * WindowSigils: Update configuration example
    
    Closes #241 (hopefully)
    
    * First attempt at labelling empty spaces
    
    Some empty spaces are unnecessarily broken up.
    
    * Add logging, setFrame()
    
    * WIP
    
    * Update docs
    
    * Remove debug
    
    * Extract CoordinateSet
    
    * Fix for __index
    
    * Split out CoordinateSet
    
    * Introduce SpaceMap
    
    * Move add_frame to SpaceMap
    
    * Move a bunch of functionality into SpaceMap
    
    * Make some methods private
    
    * Move occupied map into SpaceMap
    
    * Rename some things
    
    * Extract _mark_cells_occupied
    
    * Extract empty_rects_on_screen
    
    * Minor refactor
    
    * Refactor
    
    * Extract _find_row_right_extent
    
    * Rename some things
    
    * Simplify
    
    * Simplify more
    
    * Make North/East/South/West work with empty spaces
    
    * Extract EmptyWindow
    
    * Simplify
    
    * Implement debug dump
    
    * Ignore invisible notification center windows
    
    * Ignore some invisible windows
    
    * Blacklist Contexts app to remove unuseable windows
    
    Contexts is a window switcher that can replace or augment native OS X command tab functionality
    https://contexts.co/
    
    Co-authored-by: F <f@erbridge.co.uk>
    Co-authored-by: H <harrisontanh@gmail.com>

--- Source/WindowSigils.spoon/init.lua
+++ Source/WindowSigils.spoon/init.lua
@@ -1,11 +1,14 @@
 --- === WindowSigils ===
 ---
---- Assign every window a sigil for quick access.
+--- Assign every window and empty rectangle a sigil for quick access.
 ---
---- A letter or digit is rendered in the titlebar of every window, and actions can be bound
---- inside a "sigil" mode with different modifiers.  For example, with no modifiers, the
---- the sigil key can focus the window.  If the 'enter' action is bound to control-w, then
---- 'control-w c' will focus the window with sigil 'c'.
+--- A letter or digit is rendered in the titlebar of every window (or empty space), and
+--- actions can be bound inside the sigil mode and triggered with different modifiers.
+---
+--- For example, with no modifiers, a sigil key can focus the window.  If the 'enter'
+--- action is bound to control-w, then 'control-w c' will focus the window with sigil 'c'.
+--- The action is a function which accepts a window, or in the case of an empty space, a
+--- window-like object which reponds to 'id', 'frame', and 'setFrame'.
 ---
 --- The keys 'h', 'j', 'k', and 'l' are reserved for the window west, south, north, and
 --- east of the currently focused window in standard Vi-like fashion, and so are not
@@ -19,6 +22,37 @@
 ---
 --- Usage example:
 --- ```
+--- local function ignore_notification()
+---   -- ...
+--- end
+--- local function paste_as_keystrokes()
+---   hs.eventtap.keyStrokes(hs.pasteboard.readString())
+--- end
+--- local function rerun_last_command()
+---   hs.execute("kitty @ --to unix:/Users/jfelice/.run/kitty send-text --match=title:kak_repl_window '\x10\x0d'", true)
+--- end
+---
+--- local function focus_window(window)
+---   window:focus()
+---   if hs.window.focusedWindow() ~= window then
+---     -- Some cases with apps having windows on multiple monitors require
+---     -- us to try again (?)
+---     window:focus()
+---   end
+--- end
+---
+--- local function swap_window(window)
+---   local focused_frame = hs.window.focusedWindow():frame()
+---   local selected_frame = window:frame()
+---   hs.window.focusedWindow():setFrame(selected_frame, 0)
+---   window:setFrame(focused_frame, 0)
+--- end
+---
+--- local function stack_window(window)
+---   local frame = window:frame()
+---   hs.window.focusedWindow():setFrame(frame, 0)
+--- end
+---
 --- sigils = hs.loadSpoon("WindowSigils")
 --- sigils:configure({
 ---   hotkeys = {
@@ -31,12 +65,15 @@
 ---   },
 ---   sigil_actions = {
 ---     [{}]       = focus_window,
----     [{'ctrl'}] = swap_window,
----     [{'alt'}]  = warp_window,
+---     [{'ctrl'}] = stack_window,
+---     [{'alt'}]  = swap_window,
 ---   }
 --- })
 --- sigils:start()
 --- ```
+
+local EmptyWindow = dofile(hs.spoons.resourcePath("empty_window.lua"))
+local SpaceMap = dofile(hs.spoons.resourcePath("space_map.lua"))
 
 local obj={}
 obj.__index = obj
@@ -167,6 +204,16 @@ end
 --- Parameters:
 ---  * None
 function obj:start()
+  self.screens = hs.fnutils.map(hs.screen.allScreens(), function(screen)
+    local bounds = screen:frame()
+    local canvas = hs.canvas.new(bounds)
+    canvas:show()
+    return {
+      screen = screen,
+      canvas = canvas
+    }
+  end)
+
   self.window_filter = hs.window.filter.new({override={
     visible = true,
   }}):setDefaultFilter({
@@ -184,16 +231,6 @@ function obj:start()
     self:refresh()
   end)
 
-  self.screens = hs.fnutils.map(hs.screen.allScreens(), function(screen)
-    local bounds = screen:frame()
-    local canvas = hs.canvas.new(bounds)
-    canvas:show()
-    return {
-      screen = screen,
-      canvas = canvas
-    }
-  end)
-
   self:refresh()
 end
 
@@ -207,6 +244,37 @@ function obj:stop()
   self.window_filter = nil
 end
 
+local MINIMUM_EMPTY_SIZE = 20
+
+function obj:_addEmptySpaceWindows(windows)
+  local screens = hs.screen.allScreens()
+  local space_map = SpaceMap:new(screens, windows)
+  for _, screen in ipairs(screens) do
+    for _, frame in ipairs(space_map:empty_rects_on_screen(screen:frame())) do
+      if frame.w >= MINIMUM_EMPTY_SIZE and frame.h >= MINIMUM_EMPTY_SIZE then
+        table.insert(windows, EmptyWindow:new(frame))
+      end
+    end
+  end
+end
+
+function obj:_removeUnuseableWindows(windows)
+  return hs.fnutils.filter(windows, function(window)
+    local app = window:application()
+    local bundleID
+    if app ~= nil then
+      bundleID = app:bundleID()
+    end
+    -- On Big Sur :(
+    if bundleID == "com.apple.notificationcenterui" then return false end
+    if bundleID == "com.apple.UserNotificationCenter" then return false end
+    -- Other things
+    if bundleID == "com.pop.pop.app" and window:title() == "overlay" then return false end
+    if bundleID == "us.zoom.xos" and window:title() == "" then return false end
+    if bundleID == "com.contextsformac.Contexts" then return false end
+    return true
+  end)
+end
 
 --- WindowSigils:orderedWindows()
 --- Method
@@ -216,11 +284,17 @@ end
 ---  * None
 function obj:orderedWindows()
   local windows = self.window_filter:getWindows()
+  windows = self:_removeUnuseableWindows(windows)
+  self:_addEmptySpaceWindows(windows)
   table.sort(windows, function(a, b)
     local af, bf = a:frame(), b:frame()
     if af.x < bf.x then return true end
     if af.x > bf.x then return false end
-    return af.y < bf.y
+    if af.y < bf.y then return true end
+    if af.y > bf.y then return false end
+    -- In order to keep the sort somewhat stable, use window ids
+    local aid, bid = a:id(), b:id()
+    return aid < bid
   end)
   return windows
 end
@@ -234,24 +308,104 @@ end
 ---    Can also be 'North', 'East', 'South', or 'West' to find a window related to the
 ---    currently focused window.
 function obj:window(sigil)
+  local windows = self:orderedWindows()
   if type(sigil) == 'number' then
-    return self:orderedWindows()[sigil]
+    return windows[sigil]
   elseif sigil == 'North' then
-    return hs.window.focusedWindow():windowsToNorth(nil, true, true)[1]
+    return hs.window.focusedWindow():windowsToNorth(windows, true, true)[1]
   elseif sigil == 'East' then
-    return hs.window.focusedWindow():windowsToEast(nil, true, true)[1]
+    return hs.window.focusedWindow():windowsToEast(windows, true, true)[1]
   elseif sigil == 'South' then
-    return hs.window.focusedWindow():windowsToSouth(nil, true, true)[1]
+    return hs.window.focusedWindow():windowsToSouth(windows, true, true)[1]
   elseif sigil == 'West' then
-    return hs.window.focusedWindow():windowsToWest(nil, true, true)[1]
+    return hs.window.focusedWindow():windowsToWest(windows, true, true)[1]
   else
     for i,k in ipairs(self.sigils) do
       if k == sigil then
-        return self:orderedWindows()[i]
+        return windows[i]
       end
     end
   end
   return nil
+end
+
+
+local SIGIL_WIDTH = 20
+local SIGIL_HEIGHT = 19
+local SIGIL_MARGIN = 5
+
+function obj:_makeSigilBoxes()
+  local sigil_boxes = {}
+  local windows = self:orderedWindows()
+  for i, window in ipairs(windows) do
+    local wframe = window:frame()
+    local position = { x = wframe.x + 70, y = wframe.y + 1 }
+    table.insert(sigil_boxes, {
+      sigil = self.sigils[i],
+      position = position,
+    })
+  end
+  return sigil_boxes
+end
+
+function obj:_makeSigilElements(screen_data, sigil_boxes)
+  local bounds = screen_data.screen:frame()
+
+  local function make_frame(wframe)
+    local rect = hs.geometry.toUnitRect(wframe, bounds)
+    return { x = tostring(rect.x), y = tostring(rect.y), w = tostring(rect.w), h = tostring(rect.h) }
+  end
+
+  local function append_sigil_canvas_elements(elements, position, sigil)
+    table.insert(elements, {
+      action = "fill",
+      fillColor = { alpha = 0.3, green = 1.0, blue = 1.0 },
+      frame = make_frame{x = position.x, y = position.y, w = SIGIL_WIDTH, h = SIGIL_HEIGHT},
+      type = "rectangle",
+      withShadow = false,
+    })
+    table.insert(elements, {
+      type = "text",
+      text = sigil,
+      textFont = "Menlo Regular",
+      textSize = 18,
+      textLineBreak = 'truncateTail',
+      frame = make_frame{x = position.x + 3, y = position.y - 4, w = SIGIL_WIDTH - 3, h = SIGIL_HEIGHT + 7},
+    })
+  end
+
+  local new_elements = {}
+  for i, sigil_box in ipairs(sigil_boxes) do
+    append_sigil_canvas_elements(new_elements, sigil_box.position, sigil_box.sigil)
+  end
+  return new_elements
+end
+
+local function overlapping(box1, box2)
+  local corner1 = box1.position
+  local corner2 = box2.position
+  if corner2.y >= corner1.y + SIGIL_HEIGHT then return false end
+  if corner1.y >= corner2.y + SIGIL_HEIGHT then return false end
+  if corner2.x >= corner1.x + SIGIL_WIDTH + SIGIL_MARGIN then return false end
+  if corner1.x >= corner2.x + SIGIL_WIDTH + SIGIL_MARGIN then return false end
+  return true
+end
+
+local function move_overlapping_boxes(sigil_boxes)
+  table.sort(sigil_boxes, function (a, b)
+    if a.position.x < b.position.x then return true end
+    if a.position.x > b.position.x then return false end
+    if a.position.y < b.position.y then return true end
+    if a.position.y > b.position.y then return false end
+    return a.sigil < b.sigil
+  end)
+  for i, sigil_box in ipairs(sigil_boxes) do
+    local j = i+1
+    while j <= #sigil_boxes and overlapping(sigil_box, sigil_boxes[j]) do
+      sigil_boxes[j].position.x = sigil_box.position.x + SIGIL_WIDTH + SIGIL_MARGIN
+      j = j + 1
+    end
+  end
 end
 
 --- WindowSigils:refresh()
@@ -261,38 +415,31 @@ end
 --- Parameters:
 ---  * None
 function obj:refresh()
+  local sigil_boxes = self:_makeSigilBoxes()
+  move_overlapping_boxes(sigil_boxes)
   for _, screen_data in ipairs(self.screens) do
-    local bounds = screen_data.screen:frame()
+    local new_elements = self:_makeSigilElements(screen_data, sigil_boxes)
+    screen_data.canvas:replaceElements(new_elements)
+  end
+end
 
-    local function make_frame(wframe)
-      local rect = hs.geometry.toUnitRect(wframe, bounds)
-      return { x = tostring(rect.x), y = tostring(rect.y), w = tostring(rect.w), h = tostring(rect.h) }
-    end
-
-    local new_elements = {}
-    local windows = sigils:orderedWindows()
-    for i, window in ipairs(windows) do
-      local wframe = window:frame()
-      table.insert(new_elements, {
-        action = "fill",
-        fillColor = { alpha = 0.3, green = 1.0, blue = 1.0 },
-        frame = make_frame{x = wframe.x + 70, y = wframe.y + 1, w = 20, h = 19},
-        type = "rectangle",
-        withShadow = true,
-      })
-      table.insert(new_elements, {
-        type = "text",
-        text = self.sigils[i],
-        textFont = "Menlo Regular",
-        textSize = 18,
-        textLineBreak = 'truncateTail',
-        frame = make_frame{x = wframe.x + 73, y = wframe.y - 3, w = 17, h = 19 + 7},
-      })
-    end
-    if #new_elements > 0 then
-      screen_data.canvas:replaceElements(new_elements)
+function obj:dump()
+  local result = ""
+  for _, sigil in ipairs(self.sigils) do
+    local window = self:window(sigil)
+    if window ~= nil then
+      local app = window:application()
+      local bundleID = "<no app>"
+      if app ~= nil then
+        bundleID = app:bundleID()
+      end
+      local frame = window:frame()
+      result = string.format("%s %s) %5.0f,%5.0f,%5.0f,%5.0f  %-35s %s\n", result, sigil,
+                             frame.x1, frame.y1, frame.x2, frame.y2,
+                             bundleID, window:title())
     end
   end
+  return result
 end
 
 return obj
