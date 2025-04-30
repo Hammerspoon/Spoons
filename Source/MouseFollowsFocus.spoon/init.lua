3d943ddd31d3b88b63ec50e1e3470687b0cd09b0
Michael Mogenson <mogenson@google.com>
2025-04-30 09:57:15 -0400

    MouseFollowsFocus: avoid getting the mouse stuck in screen corners
    The mouse cursor moves in a straight line between its current position
    and the destination set with hs.mouse.absolutePosition(). If the mouse
    is moving to a new screen, it can get caught on a corner that does not
    connect between screens, depending on the screen sizes and layout.
    
    When moving the mouse between screens, first move the mouse to the
    center of the current screen, then to the center of the new screen, then
    the target destination. This path has the best chance at avoiding any
    non-connected edges between screens.
    
    These extra mouse movements happen instantaneously and are not visible
    to the user.

--- Source/MouseFollowsFocus.spoon/init.lua
+++ Source/MouseFollowsFocus.spoon/init.lua
@@ -55,7 +55,7 @@ function obj:start()
   })
   self.window_filter:subscribe({
     hs.window.filter.windowFocused
-  }, function(window) 
+  }, function(window)
       if self.onChangeOfScreenOnly and self.currentWindowScreen and self.currentWindowScreen:id() == window:screen():id() then return end
       self:updateMouse(window)
       self.currentWindowScreen = window:screen()
@@ -91,6 +91,13 @@ function obj:updateMouse(window)
   local current_pos = hs.geometry(hs.mouse.absolutePosition())
   local frame = window:frame()
   if not current_pos:inside(frame) then
+    local current_screen = hs.mouse.getCurrentScreen()
+    local window_screen = window:screen()
+    if current_screen and window_screen and current_screen ~= window_screen then
+      -- avoid getting the mouse stuck on a screen corner by moving through the center of each screen
+      hs.mouse.absolutePosition(current_screen:frame().center)
+      hs.mouse.absolutePosition(window_screen:frame().center)
+    end
     hs.mouse.absolutePosition(frame.center)
   end
 end
